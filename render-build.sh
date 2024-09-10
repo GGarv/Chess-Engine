61ac9d566a5903ef085f9639302efee7bda3b59a
Garv Gupta <120120156+GGarv@users.noreply.github.com>
2024-09-10 17:34:07 +0530

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Install a precompiled Pygame wheel
-pip install https://www.pygame.org/ftp/pygame-2.0.1-cp39-cp39-manylinux1_x86_64.whl
-
-# Install other Python dependencies
-pip install -r requirements.txt
